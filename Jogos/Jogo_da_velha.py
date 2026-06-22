95fed0369796d7d9609a77f31010d905a4e79ab6
Mateus Coelho Miranda-lang <martcoelhomiranda@gmail.com>
2026-06-22 15:03:45 -0300

    Jogo final

--- Jogos/Jogo_da_velha.py
+++ Jogos/Jogo_da_velha.py
@@ -11,35 +11,29 @@ def jogada_jogador():
         print("Lugar já ocupado, tente novamente!")
         return jogada_jogador()
 
-def jogada_maquina(rodada_maquina):
-    rodada_maquina += 1
+def jogada_maquina():
+    jogada = encontrar_jogada_vencedora(simbolo_maquina)
+    if jogada:
+        tabuleiro[jogada[0]][jogada[1]] = simbolo_maquina
+        return jogada[0], jogada[1]
 
-    if rodada_maquina <= 1:
-        linha, coluna = randomizar_jogada()
-    else:
-        jogada = encontrar_jogada_vencedora(simbolo_maquina)
-        if jogada:
-            tabuleiro[jogada[0]][jogada[1]] = simbolo_maquina
-            linha, coluna = jogada[0], jogada[1]
-        else:
-            jogada = encontrar_jogada_vencedora(simbolo_jogador)
-            if jogada:
-                tabuleiro[jogada[0]][jogada[1]] = simbolo_maquina
-                linha, coluna = jogada[0], jogada[1]
-            else:
-                linha, coluna = randomizar_jogada()
+    jogada = encontrar_jogada_vencedora(simbolo_jogador)
+    if jogada:
+        tabuleiro[jogada[0]][jogada[1]] = simbolo_maquina
+        return jogada[0], jogada[1]
 
-    return linha, coluna, rodada_maquina
+    cantos = [[0,0], [2,2], [0,2], [2,0]]
+    random.shuffle(cantos)
+    for canto in cantos:
+        if not tabuleiro[canto[0]][canto[1]]:
+            tabuleiro[canto[0]][canto[1]] = simbolo_maquina
+            return canto[0], canto[1]
 
-def randomizar_jogada():
-    linha = random.randint(0, 2)
-    coluna = random.randint(0, 2)
-
-    if not tabuleiro[linha][coluna]:
-        tabuleiro[linha][coluna] = simbolo_maquina
-        return linha, coluna
-    else:
-        return randomizar_jogada()
+    for i in range(3):
+        for j in range(3):
+            if tabuleiro[i][j] == 0:
+                tabuleiro[i][j] = simbolo_maquina
+                return i, j
 
 def encontrar_jogada_vencedora(jogador):
     for i in range(3):
@@ -119,13 +113,15 @@ def imprimir_tabuleiro():
             print(simbolos[tabuleiro[i][j]], end=" ")
         print()
 
-# VARIAVEIS
-vencedor = 0
-rodada_maquina = 0
-turno = random.randint(1, 2)
-placar_velha = 0
+def resetar_tabuleiro():
+    for i in range(3):
+        for j in range(3):
+            tabuleiro[i][j] = 0
+
 placar_vitoria_j = 0
 placar_vitoria_m = 0
+placar_velha = 0
+jogar_de_novo = True
 
 tabuleiro = [
     [0, 0, 0],
@@ -133,53 +129,50 @@ tabuleiro = [
     [0, 0, 0]
 ]
 
-if turno == 1:
-    print("Você começa! Você é o X")
-    simbolo_jogador = 1
-    simbolo_maquina = 2
-    simbolos = {0: ".", 1: "X", 2: "O"}
-else:
-    print("Máquina começa! Você é o O")
-    simbolo_jogador = 2
-    simbolo_maquina = 1
-    simbolos = {0: ".", 1: "X", 2: "O"}
+while jogar_de_novo == True :
+    vencedor = 0
+    turno = 2
+    resetar_tabuleiro()
 
-imprimir_tabuleiro()
-
-# WHILE
-while vencedor == 0:
     if turno == 1:
-        linha, coluna = jogada_jogador()
-        print("Sua jogada:")
-        imprimir_tabuleiro()
-        if verificar_possibilidade(linha, coluna, simbolo_jogador):
-            vencedor = 1
-            print("Você ganhou!")
-            placar_vitoria_j+=1
-        turno = 2
+        print("Você começa! Você é o X")
+        simbolo_jogador = 1
+        simbolo_maquina = 2
+        simbolos = {0: ".", 1: "X", 2: "O"}
     else:
-        print("Jogada da máquina:")
-        linha, coluna, rodada_maquina = jogada_maquina(rodada_maquina)
-        imprimir_tabuleiro()
-        if verificar_possibilidade(linha, coluna, simbolo_maquina):
-            vencedor = 2
-            print("Máquina ganhou!")
-            placar_vitoria_m+=1
-        turno = 1
+        print("Máquina começa! Você é o O")
+        simbolo_jogador = 2
+        simbolo_maquina = 1
+        simbolos = {0: ".", 1: "X", 2: "O"}
 
-    if verificar_velha() and vencedor == 0:
-        vencedor = 3
-        print("Velha! Mais sorte na proxima vez!")
-        placar_velha+= 1
+    imprimir_tabuleiro()
 
+    while vencedor == 0:
+        if turno == 1:
+            linha, coluna = jogada_jogador()
+            print("Sua jogada:")
+            imprimir_tabuleiro()
+            if verificar_possibilidade(linha, coluna, simbolo_jogador):
+                vencedor = 1
+                print("Você ganhou!")
+                placar_vitoria_j += 1
+            turno = 2
+        else:
+            print("Jogada da máquina:")
+            linha, coluna = jogada_maquina()
+            imprimir_tabuleiro()
+            if verificar_possibilidade(linha, coluna, simbolo_maquina):
+                vencedor = 2
+                print("Máquina ganhou!")
+                placar_vitoria_m += 1
+            turno = 1
 
-
-
-
-
-
-
-
-
-
-
+        if verificar_velha() and vencedor == 0:
+            vencedor = 3
+            print("Velha!")
+            placar_velha += 1
+    print()
+    print(f"Placar - Você: {placar_vitoria_j} | Máquina: {placar_vitoria_m} | Velhas: {placar_velha}")
+    resposta = input("Quer jogar de novo? (s/n): ")
+    if resposta != "s":
+        jogar_de_novo = False
